96861636cb7f997e48ca78bc019be414b9ee964e
root <root@DESKTOP-VJAS3QG.localdomain>
2024-04-07 11:33:00 -0600

    PrincipioComponen

--- src/Componentes/ButtonSection.jsx
+++ src/Componentes/ButtonSection.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import './ButtonSection.css'
 
 function ButtonSection() {
     return (
-        <div class="button-container">
-            <button >GitHub</button>
-            <button>Frontend Mentor</button>
-            <button>LinkedIn</button>
-            <button>Twitter</button>
-            <button>Instagram</button>
+        <div className='container'>
+            <div class="button-container">
+                <button >GitHub</button>
+                <button>Frontend Mentor</button>
+                <button>LinkedIn</button>
+                <button>Twitter</button>
+                <button>Instagram</button>
+            </div>
         </div>
+
     )
 }
 
--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import UserDetails from './UserDetails.jsx'
+import ButtonSection from './ButtonSection.jsx'
 
 function Card() {
     return (
         <div>
             <UserDetails/> 
-                
+            <ButtonSection/>
         </div>
     )
 }
