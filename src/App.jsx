b8fe7c871361695a35b83505cf49e577295c9491
root <root@DESKTOP-VJAS3QG.localdomain>
2024-04-17 17:21:42 -0400

    FinalRick

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import './App.css'
-import Card from './Componentes/Card';
+import './App.css';
 
 function App() {
-  const [users, setusers] = useState([])  //Arive con [],   user es un arrglo se itera
+  const [characters, setCharacters] = useState([]);
+
+  function generarNumerosAleatorios() {
+    const numeros = [];
+    for (let i = 0; i < 10; i++) {
+      numeros.push(Math.floor(Math.random() * 857));
+    }
+    numeros.toString();
+    return numeros;
+  }
   useEffect(() => {
-    //first Cuando entre los datos pedir datos
-    fetch('https://6617cc6fed6b8fa43483cfab.mockapi.io/Api/Version1/users')//esto es un get (Es una promesa-es asigcronica)
-      .then(Respuesta => {  //variable 
-        return Respuesta.json();
+    fetch(
+      "https://rickandmortyapi.com/api/character/" + generarNumerosAleatorios()
+    )
+      .then((res) => {
+        return res.json();
       })
-      .then(Data => {
-        setusers(Data)
-        console.log(Data)  //Sin comilla ya que es una variable
-      }) 
-  }, []) //Una vez q aparecio
+      .then((data) => {
+        setCharacters(data);
+      });
+  }, []);
+  // const getApi = async () => {
+  //   try {
+  //     const res = await fetch('https://rickandmortyapi.com/api/character');
+  //     const data = await res.json();
+  //     setCharacters(data.results);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getApi();
+  // }, []);
 
   return (
-      <div>
-        {
-          users.map((user) =>
-            (<Card key={user.id} user={user} />)
-          ) 
-        }
-      </div>
-  )
+    <div className="container">
+      {characters.map((character) => (
+        <div key={character.id} className="card">
+          <h2 className="Names">{character.id}. {character.name}</h2>
+          <img className="Imagen" src={character.image} alt="" />
+          <p className="SubT">Estado - Especie:</p>
+          <p className="Espe">{character.status} - {character.species}</p>
+          <p className="SubT">Ultima ubicacion conocida:</p>
+          <p className="Local">{character.location.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default App
+export default App;
