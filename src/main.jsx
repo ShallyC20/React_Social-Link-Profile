45f8d3db0a0be3633e9f2487c816517d337b480d
root <root@DESKTOP-VJAS3QG.localdomain>
2024-04-25 07:59:21 -0400

    FinGhpages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React ,{createHashRouter, RouterProvider}from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './Mesa1440.css'
+import PrincipalRick from './Componentes/PrincipalRick.jsx'
+import FichasRick from './Componentes/FichasRick.jsx'
+
+const router =createHashRouter([
+  {
+    path:"/",
+    element:<PrincipalRick />
+  },
+  {
+    ppath:"/",
+    element:<FichasRick />
+  },
+  {
+    path:"/",
+    element:<p>ERROR 404 Pagina inexitente</p>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "htpps:shallyc20.github.io/React_Social-Link-Profile/", // social-links-profile
 })
